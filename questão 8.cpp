96b6d8a01deab11dd0d9e5b4e4c0b27285c21476
ianne01ssousa <iannerayla60@gmail.com>
2025-07-03 08:04:43 -0300

    Create questão 8.cpp

---
+++ questão 8.cpp
@@ -0,0 +1,11 @@
+//example program
+#include <iostream>
+using namespace std;
+   void mostrarMensagem(){
+       cout<<"Seja bem vindo.";
+   }
+
+int main() {
+    mostrarMensagem();
+    return 0;
+}
