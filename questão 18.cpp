44dc1dc46fbd8f74a90f973b73db4e7de53f5710
ianne01ssousa <iannerayla60@gmail.com>
2025-07-08 16:29:15 -0300

    Update questão 18.cpp

--- questão 18.cpp
+++ questão 18.cpp
@@ -1,16 +1,17 @@
+//exemp program
 #include <iostream>
 using namespace std;
-  void incrementar (int *p){
-      *p = *p + 1;
-      cout<<"O valor apos a incrementaçao é "<< *p<<endl;
-  }
+
+void incrementar(int *p) {
+    *p = *p + 1;
+    cout << "O valor apos a incrementacao é " << *p << endl;
+}
 
 int main() {
     int num1;
-    cout<<"Informe um valor: "<<endl;
-    cin>> num1;
-    int ptr = num1;
-    
-    incrementar(&ptr);
+    cout << "Informe um valor: " << endl;
+    cin >> num1;
+
+    incrementar(&num1);
     return 0;
 }
