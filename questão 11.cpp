1f57f0abf96517c771355c584fc6e07ee29f92cd
ianne01ssousa <iannerayla60@gmail.com>
2025-07-08 16:25:58 -0300

    Update questão 11.cpp

--- questão 11.cpp
+++ questão 11.cpp
@@ -4,10 +4,10 @@ using namespace std;
 
 int main() {
     int num1 = 10;
-    int *ptr;    
-    ptr = &a;
-    
-    cout<< " Valor da variavel: "<<a <<endl;
-    cout<<" Endereço da variavel: "<<ptr <<endl;
+    int *ptr;
+    ptr = &num1;
+
+    cout << "Valor da variavel: " << num1 << endl;
+    cout << "Endereco da variavel: " << ptr << endl;
     return 0;
 }
